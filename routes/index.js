75c753b33cd1880f97e392cd828aea3e422f3841
viniii05 <vinivt.0520@gmail.com>
2024-08-13 00:59:31 +0530

    meeting management app (fetching booked slot not working)

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,9 @@
-// routes/index.js
 
 const express = require('express');
 const router = express.Router();
 
 const slotRoutes = require('./slots');
 
-// Use routes
 router.use('/', slotRoutes);
 
 module.exports = router;
